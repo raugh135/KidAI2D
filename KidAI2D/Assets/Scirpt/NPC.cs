bedbb1c6d381541f14cbfacd9adc905a8f0042ef
raugh135 <55518299+raugh135@users.noreply.github.com>
2019-10-03 11:42:11 +0800

    NPC1003

---
+++ KidAI2D/Assets/Scirpt/NPC.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+    [Header("對話")]
+    public string SayStart = "嗨,我請你可以幫我嗎?";
+    public string SayNotComplete = "你還沒找到十顆櫻桃呢...?";
+    public string SayComplete = "感謝你幫我找到十顆櫻桃~";
+
+    [Header("對話速度")]
+    public float Speed = 1.5f;
+
+    [Header("任務相關")]
+    public bool Complete = false;
+    public int CountPlayer = 0;
+    public int CountFinish = 10;
+}
+	
+
