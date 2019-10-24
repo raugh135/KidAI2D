29ef21d705795434c4f24b5442fba610cf78cc22
raugh135 <55518299+raugh135@users.noreply.github.com>
2019-10-24 12:09:18 +0800

    2Dkid

--- KidAI2D/Assets/Scirpt/Fox.cs
+++ KidAI2D/Assets/Scirpt/Fox.cs
@@ -1,13 +1,32 @@
 ﻿
 using UnityEngine;
 
-public class Fox : MonoBehaviour {
+public class Fox : MonoBehaviour
+{
 
-    public int speed;
+    public int speed = 50;
+    public float jump = 50;
+    public string foxName = "玩家";
+    public bool pass = false;
 
+    private Rigidbody2D r2d;
+    private Transform tran;
 
     private void Start()
     {
-        Debug.Log("OMG!");
+        tran = GetComponent<Transform>();
+        r2d = GetComponent<Rigidbody2D>();
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) tran.eulerAngles = new Vector3(0, 0, 0);
+        if (Input.GetKeyDown(KeyCode.A)) tran.eulerAngles = new Vector3(0, 180, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        r2d.AddForce(new Vector2(speed * Input.GetAxis("Horizontal"), 0));
+    }
+
 }
