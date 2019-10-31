fe848841070f3c26bf14b1664eac3d16c8cf339a
raugh135 <55518299+raugh135@users.noreply.github.com>
2019-10-31 12:08:32 +0800

    10-31-2D

--- KidAI2D/Assets/Scirpt/Fox.cs
+++ KidAI2D/Assets/Scirpt/Fox.cs
@@ -8,6 +8,7 @@ public class Fox : MonoBehaviour
     public float jump = 50;
     public string foxName = "玩家";
     public bool pass = false;
+    public bool isGround;
 
     private Rigidbody2D r2d;
     private Transform tran;
@@ -20,13 +21,45 @@ public class Fox : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D)) tran.eulerAngles = new Vector3(0, 0, 0);
-        if (Input.GetKeyDown(KeyCode.A)) tran.eulerAngles = new Vector3(0, 180, 0);
+        if (Input.GetKeyDown(KeyCode.D)) Turn(0);
+        if (Input.GetKeyDown(KeyCode.A)) Turn(180);
     }
 
     private void FixedUpdate()
     {
+        Walk();
+        Jump();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGround = true;
+        Debug.Log("碰到東西" + collision.gameObject);
+    }
+
+    /// <summary>
+    /// 走路
+    /// </summary>
+    private void Walk()
+    {
         r2d.AddForce(new Vector2(speed * Input.GetAxis("Horizontal"), 0));
     }
 
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            isGround = false;
+            r2d.AddForce(new Vector2(0, jump));
+        }
+    }
+
+    /// <summary>
+    /// 轉彎
+    /// </summary>
+    /// <param name="direction">轉彎:左 180,右 0 </param>
+    private void Turn( int direction)
+    {
+        tran.eulerAngles = new Vector3(0, direction, 0);
+    }
 }
